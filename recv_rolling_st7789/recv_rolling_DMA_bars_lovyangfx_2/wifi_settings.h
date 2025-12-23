27c5bd4d8148eb2f856687d49bbfe3fb1db07c85
piotrcurious <78758456+piotrcurious@users.noreply.github.com>
2025-12-23 14:53:58 +0100

    works and works much better.

---
+++ recv_rolling_st7789/recv_rolling_DMA_bars_lovyangfx_2/wifi_settings.h
@@ -0,0 +1,15 @@
+#include <WiFi.h>
+#include <AsyncUDP.h>
+#include <esp_wifi.h>
+
+const char* ssid = "injection";
+const char* password = "irrolling12";
+const uint8_t channel = 1;
+const bool hidden = 0;
+const uint8_t max_connection = 8;
+const uint16_t beacon_interval = 500;
+const uint16_t timeout_period = 5000; // expected delay inbetween packets (for accounting display)
+//#define AP_mode_on 1 ; // set to 1 to compile AP node
+
+uint16_t multicastPort = 5683;  // local port to listen on
+IPAddress multicastIP(224,0,1,187);
